bf12ffb62dc21e321a4faee26c00153a5f5a2c61
zeta0ne <space1signs@gmail.com>
2020-11-12 17:22:13 +0300

    10 methods done, all tests pass, 1 negative test pass

--- DataStructures.Tests/ArrayListTests.cs
+++ DataStructures.Tests/ArrayListTests.cs
@@ -5,7 +5,7 @@ using DataStructures;
 namespace DataStructures.Tests
 {
     public class ArrayListTests
-    {
+    {   //конструктор можно не тестировать
         
         [TestCase(33, new int[3] { 1, 2, 3 }, new int[4] { 1, 2, 3, 33 } )]
         [TestCase(33, new int[0] { }, new int[1] { 33 })]
@@ -17,9 +17,9 @@ namespace DataStructures.Tests
             Assert.AreEqual(expected, actual);
         }
 
-        [TestCase(33, new int[3] { 1, 2, 3 }, new int[4] { 33, 1, 2, 3 })]
-        [TestCase(33, new int[0] { }, new int[1] { 33 })]
-        [TestCase(33, new int[3] { 0, 0, 0 }, new int[3] { 33, 0, 0 })]
+        [TestCase(33, new int[] { 1, 2, 3 }, new int[] { 33, 1, 2, 3 })]
+        [TestCase(33, new int[] { }, new int[] { 33 })]
+        [TestCase(33, new int[3] { 0, 0, 0 }, new int[4] { 33, 0, 0, 0 })]
         public void AddToFirstTest(int value, int[] array, int[] expArr)
         {
             ArrayList expected = new ArrayList(expArr);
@@ -27,5 +27,84 @@ namespace DataStructures.Tests
             actual.AddToFirst(value);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestCase(2, 7, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 7, 3, 4, 5 })]
+        [TestCase(0, 33, new int[] { }, new int[] { 33 })]
+        [TestCase(0, 33, new int[3] { 0, 0, 0 }, new int[4] { 33, 0, 0, 0 })]
+        public void AddToIndexTest(int index, int value, int[] array, int[] expArr)
+        {
+            ArrayList expected = new ArrayList(expArr);
+            ArrayList actual = new ArrayList(array);
+            actual.AddToIndex(index, value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(2, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 4, 5 })]
+        [TestCase(0, new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 3, 4, 5 })]
+        [TestCase(4, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4 })]
+        [TestCase(0, new int[] { 1 }, new int[0] { })]
+        public void RemoveTest(int index, int[] array, int[]expArr)
+        {
+            ArrayList expected = new ArrayList(expArr);
+            ArrayList actual = new ArrayList(array);
+            actual.RemoveIndex(index);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4 })]
+        [TestCase(new int[] { 1 }, new int[] { })]
+        public void RemoveLastTest(int[] array, int[] expArr)
+        {
+            ArrayList expected = new ArrayList(expArr);
+            ArrayList actual = new ArrayList(array);
+            actual.RemoveLast();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[0] { }, 0, 0)]
+        public void GetLengthTest(int[] array, int expected, int actual)
+        {
+            ArrayList arr = new ArrayList(array);
+            actual = arr.GetLength(array);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4 }, 2, 3)]
+        public void GetValueByIndexTest(int[] array, int index, int expected )
+        {
+            ArrayList arr = new ArrayList(array);
+            int actual = arr.GetValueByIndex(index);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4 }, 3, 2)]
+        public void GetIndexByValueTest(int[] array, int value, int expected)
+        {
+            ArrayList arr = new ArrayList(array);
+            int actual = arr.GetIndexByValue(value);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 3, 8, new int[] { 1, 2, 3, 8, 5 })]
+        
+        public void ChangeValueByIndexTest(int[] array, int index, int value, int[] expArr)
+        {
+            ArrayList expected = new ArrayList(expArr);
+            ArrayList actual = new ArrayList(array);
+            actual.ChangeValueByIndex(index, value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { }, 0, 8, new int[] { 8 })]
+        public void ChangeValueByIndexTestNegative(int[] array, int index, int value, int[] expArr)
+        {
+            ArrayList expected = new ArrayList(expArr);
+            ArrayList actual = new ArrayList(array);
+            
+            Assert.Throws <IndexOutOfRangeException> (()=>
+            {
+                actual.ChangeValueByIndex(index, value);
+            });
+
+        }
+
     }
 }
--- DataStructures/ArrayList.cs
+++ DataStructures/ArrayList.cs
@@ -6,9 +6,11 @@ namespace DataStructures
     public class ArrayList
     {
         private int[] _array; //поле
+        //вынести 1.33 в константу
         public int Length { get; private set; } //автоматическое свойство чтобы обращаться к полезной длине
 
         public ArrayList(int[] array) //конструктор 
+            //конструктор создаёт пустой массив
         {
             _array = new int[(int)(array.Length * 1.33d)]; //умножение на 1.33 чтобы размер был больше 0
             Length = array.Length;
@@ -38,24 +40,111 @@ namespace DataStructures
 
         public void AddToFirst(int value)
         {
+
             if (Length <= _array.Length)
             {
                 IncreaseLength();
-                
             }
             int[] newArr = new int[_array.Length];
             for (int i = 0; i <= Length; i++)
             {
                 newArr[i+1] = _array[i];
             }
+            Length++;
             newArr[0] = value;
             _array = newArr;
         }
 
         public void AddToIndex(int index, int value)
         {
+            if (Length <= _array.Length)
+            {
+                IncreaseLength();
+            }
+            if (Length != 0)
+            {
+                for (int i = Length; i >= Length - index; i--)
+                {
+                    _array[i] = _array[i - 1];
+                }
+            }
+            Length++;
+            _array[index] = value;
+
+            //int[] newArr = new int[_array.Length];
+            //Length++;
+            //for (int i = 0; i < index; i++)
+            //{
+            //    newArr[i] = _array[i];
+            //}
+            //for (int i = index; i <= Length; i++)
+            //{
+            //    if (i <= _array.Length)
+            //    {
+            //        newArr[i + 1] = _array[i];
+            //    }
+            //}
+            //newArr[index] = value;
+            //_array = newArr;
+        }
+
+        public void RemoveFirst()
+        {
+            RemoveIndex(0);
+        }
+        public void RemoveLast()
+        {
+            RemoveIndex(Length);
+        }
+
+        //make private?
+        public void RemoveIndex(int index)
+        {
+            if (Length > 1)
+            {
+                for (int i = index; i < Length; i++)
+                {
+                    _array[i] = _array[i + 1];
+                }
+            }
+            Length--;
+        }
+
+        public int GetLength(int[] array)
+        {
+            return Length;
+        }
+
+        public int GetValueByIndex(int index)
+        {
+            return _array[index];
+        }
+
+        public int GetIndexByValue(int value)
+        {
+            int index = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] == value)
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public void ChangeValueByIndex(int index, int value)
+        {
+            _array[index] = value;
+
+            if (index > Length || index < 0 )
+            {
+                throw new IndexOutOfRangeException();
+            }
 
         }
+
+
 
         private void IncreaseLength(int number = 1)
         {
