13046121c5975f22ea3d780dce9a3ec32e489476
zeta0ne <space1signs@gmail.com>
2020-11-14 18:06:12 +0300

    3 methods for adding added

---
+++ DataStructures.Tests/LinkedListTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using DataStructures.LL;
+
+namespace DataStructures.Tests
+{
+    public class LinkedListTests
+    {
+        [TestCase(new int[] { 1, 2, 3 }, 1, 4, new int[] { 1, 4, 2, 3 })]
+        [TestCase(new int[] { 1, 2, 3 }, 0, 4, new int[] { 4, 1, 2, 3 })]
+        [TestCase(new int[] { 1, 2, 3 }, 3, 4, new int[] { 1, 2, 3, 4 })]
+        [TestCase(new int[] { }, 0, 4, new int[] { 4 })]
+        [TestCase(new int[] { 1 }, 0, 4, new int[] { 4, 1 })]
+        public void AddByIndexTest(int[] array, int index, int value, int[] expArr)
+        {
+            LinkedList expected = new LinkedList(expArr);
+            LinkedList actual = new LinkedList(array);
+
+            actual.AddByIndex(index, value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 2, 3 }, 4, new int[] { 4, 1, 2, 3 })]
+        [TestCase(new int[] {  }, 4, new int[] { 4 })]
+        [TestCase(new int[] { 0 }, 4, new int[] { 4, 0 })]
+        public void AddToBeginnigTest(int[] array, int value, int[] expArr)
+        {
+            LinkedList expected = new LinkedList(expArr);
+            LinkedList actual = new LinkedList(array);
+
+            actual.AddToBeginning(value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 2, 3 }, 4, new int[] { 1, 2, 3, 4 })]
+        [TestCase(new int[] {  }, 4, new int[] { 4 })]
+        public void AddToEndTest(int[] array, int value, int[] expArr)
+        {
+            LinkedList expected = new LinkedList(expArr);
+            LinkedList actual = new LinkedList(array);
+
+            actual.AddToEnd(value);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- DataStructures/LL/LinkedList.cs
+++ DataStructures/LL/LinkedList.cs
@@ -9,6 +9,31 @@ namespace DataStructures.LL
         public int Length { get; set; }
         
         private Node _root; //ссылка на первый элемент списка
+                            //рут - не нода, а место для хранения ссылки
+                            //это переменная, а не объект
+
+        public int this[int index]
+        {
+            get //достаёт значение по индексу
+            {
+                Node tmp = _root; //ссылка которая лежит в тмп теперь такая же как в руте
+                for (int i = 1; i <= index; i++) //не с нуля, потому что нулевой элемент и так в руте/тмп
+                {
+                    tmp = tmp.Next; 
+                }
+                return tmp.Value;
+            }
+
+            set //с помощью сеттера можно менять значение по индексу
+            {
+                Node tmp = _root; 
+                for (int i = 1; i <= index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                tmp.Value = value;
+            }
+        }
 
         public LinkedList() //пустой конструктор ЛЛ не должен обращаться к руту, 
                             //потому что если в руте будет ссылка на какой-то объект, 
@@ -27,19 +52,125 @@ namespace DataStructures.LL
 
         public LinkedList(int[] array) //конструктор на основе массива
         {
-            _root = new Node(array[0]); //рут - первый элемент, поэтому он с индексом 0
-            Node tmp = _root; //временная переменная для счёта, ссылается туда же куда и рут
-
-            for (int i = 1; i < array.Length; i++)
+            if (array.Length != 0) //на случай если передаётся пустой массив
             {
-                tmp.Next = new Node(array[i]); //tmp.Next обращается к полю для хранения ссылки внутри ноды;
-                                               //1) создаётся новый объект типа нод
-                                               //2) ссылка на новый объект возвращается в tmp.Next
-                                               //3) некст это поле для хранения ссылки
+                _root = new Node(array[0]); //рут - первый элемент, поэтому он с индексом 0
+                Node tmp = _root; //временная переменная для счёта, ссылается туда же куда и рут
 
-                tmp = tmp.Next; //переопределение временной переменной в следующий элемент
+                for (int i = 1; i < array.Length; i++)
+                {
+                    tmp.Next = new Node(array[i]); //tmp.Next обращается к полю для хранения ссылки внутри ноды;
+                                                   //1) создаётся новый объект типа нод
+                                                   //2) ссылка на новый объект возвращается в tmp.Next
+                                                   //3) некст это поле для хранения ссылки
+
+                    tmp = tmp.Next; //переопределение временной переменной в следующий элемент
+                }
+                Length = array.Length;
             }
-            Length = array.Length;
+            else
+            {
+                Length = 0;
+                _root = null;
+            }
         }
+
+        public void AddToEnd(int value)
+        {
+            if (Length == 0) //отдельная обработка пустого списка
+            {
+                _root = new Node(value); 
+            }
+            else
+            {
+                Node current = _root; //временная переменная чтобы от начала списка бежать по нему до конца
+                for (int i = 1; i < Length; i++) //нужно прибежать на одну клетку раньше, чтобы с этой клетки кинуть ссылку на новый нод
+                {
+                    current = current.Next;
+                }
+                current.Next = new Node(value); //новый нод с нужным значением на месте следующего за тем, до которого мы дошли в цикле
+                                                //в каррент.Некст теперь лежит ссылка на новый элемент
+            }
+            Length++;
+        }
+
+        public void AddToBeginning(int value)
+        {
+            Node tmp = _root; //сохраняем рут во временную переменную чтобы не потерять
+            _root = new Node(value); //новый рут
+            _root.Next = tmp; //новый рут теперь ссылается на старый
+            Length++;
+        }
+
+
+        public void AddByIndex(int index, int value)
+        {
+            if (index == 0) //отдельная обработка рута
+            {
+                Node tmp = _root; //сохраняем рут во временную переменную чтобы не потерять
+                _root = new Node(value); //новый рут
+                _root.Next = tmp; //новый рут теперь ссылается на старый
+            }
+            else
+            {
+                Node current = _root; //временная переменная чтобы от начала списка бежать по нему до определённого индекса
+                for (int i = 1; i < index; i++) //нужно прибежать на одну клетку раньше, чтобы с этой клетки кинуть ссылку на новый нод
+                {
+                    current = current.Next;
+                }
+                Node tmp = current.Next; //временная переменная чтобы сохранить ссылку на элемент который будет после добавленного
+                current.Next = new Node(value); //новый нод с нужным значением на месте следующего за тем, до которого мы дошли в цикле
+                                                //в каррент.Некст теперь лежит ссылка на новый элемент
+
+                //current - ссылка на элемент перед нужным индексом, на котором остановился цикл
+                //каррент.Некст - ссылка на теперь уже новый нод
+                //каррент.Некст.Некст = тмп - в следующий элемент кидается ссылка из тмп на весь оставшийся список
+                current.Next.Next = tmp;
+            }
+            Length++;
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+            LinkedList linkedList = (LinkedList)obj;
+
+            if (Length != linkedList.Length)
+            {
+                return false;
+            }
+
+            Node tmp1 = _root;
+            Node tmp2 = linkedList._root;
+            for (int i = 0; i < Length; i++)
+            {
+                if (tmp1.Value != tmp2.Value)
+                {
+                    return false;
+                }
+                tmp1 = tmp1.Next;
+                tmp2 = tmp2.Next;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+
+            if (_root != null)
+            {
+                Node tmp = _root;
+
+                while (tmp != null)
+                {
+                    s += tmp.Value + ";";
+                    tmp = tmp.Next;
+                }
+            }
+            return s;
+        }
+
     }
 }
